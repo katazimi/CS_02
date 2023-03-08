d560da7cd55fc453fbbdebfccb4c59f8102b8d86
katazimi <127263637+katazimi@users.noreply.github.com>
2023-03-08 10:20:47 +0900

    2023-03-08

---
+++ 001_BMI/001_BMI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_BMI
+{
+    internal class BMI
+    {
+        static void Main(string[] args)
+        {
+            double height, weight;
+            string blank;
+
+            Console.Write("키(cm) : ");
+            height = double.Parse(Console.ReadLine());
+
+            Console.Write("체중(kg) : ");
+            weight = double.Parse(Console.ReadLine());
+
+            double bmi = weight / (height / 100 * height / 100);
+
+            //Console.WriteLine("BMI = " +  bmi + "입니다.");
+            Console.WriteLine("BMI = {0}입니다.", bmi);
+            //문자열 + 숫자 => 문자열
+            //문자열 + 문자열 => 문자열
+            Console.WriteLine("아무키나 입력하세요");
+            blank = Console.ReadLine();
+        }
+    }
+}
---
+++ 002_BMI/002_BMI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _002_BMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double height = double.Parse(textBox1.Text);
+            double weight = double.Parse(textBox2.Text);
+
+            double bmi = weight / (height / 100 * height / 100);
+            label3.Text = "BMI = " + bmi + "입니다.";
+            label3.Text = String.Format("BMI = {0}입니다.", bmi);
+            label4.Text
+                = string.Format("BMI = {0}입니다.", bmi);
+        }
+    }
+}
